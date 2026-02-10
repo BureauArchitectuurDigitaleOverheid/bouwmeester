802ae10c2a741846d12125bb7a7ff73a6b8eccc0
Anne Schuth <anne.schuth@rijksoverheid.nl>
2026-02-10 11:50:58 +0100

    Fix ruff lint errors in migration file

--- backend/bouwmeester/migrations/versions/492a105cdce5_add_http_sessions_table.py
+++ backend/bouwmeester/migrations/versions/492a105cdce5_add_http_sessions_table.py
@@ -5,28 +5,35 @@ Revises: 94e112019e64
 Create Date: 2026-02-10 11:22:41.398588
 
 """
-from typing import Sequence, Union
 
-from alembic import op
+from collections.abc import Sequence
+
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = '492a105cdce5'
-down_revision: Union[str, None] = '94e112019e64'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision: str = "492a105cdce5"
+down_revision: str | None = "94e112019e64"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
-    op.create_table('http_sessions',
-    sa.Column('session_id', sa.String(length=64), nullable=False),
-    sa.Column('data', sa.Text(), nullable=False),
-    sa.Column('expires_at', sa.DateTime(timezone=True), nullable=False),
-    sa.PrimaryKeyConstraint('session_id')
+    op.create_table(
+        "http_sessions",
+        sa.Column("session_id", sa.String(length=64), nullable=False),
+        sa.Column("data", sa.Text(), nullable=False),
+        sa.Column("expires_at", sa.DateTime(timezone=True), nullable=False),
+        sa.PrimaryKeyConstraint("session_id"),
     )
-    op.create_index(op.f('ix_http_sessions_expires_at'), 'http_sessions', ['expires_at'], unique=False)
+    op.create_index(
+        op.f("ix_http_sessions_expires_at"),
+        "http_sessions",
+        ["expires_at"],
+        unique=False,
+    )
 
 
 def downgrade() -> None:
-    op.drop_index(op.f('ix_http_sessions_expires_at'), table_name='http_sessions')
-    op.drop_table('http_sessions')
+    op.drop_index(op.f("ix_http_sessions_expires_at"), table_name="http_sessions")
+    op.drop_table("http_sessions")
